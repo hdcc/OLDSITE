7f465a07cb080e5433a72e3dda76ea34db2af746
hdcc <dwilliams@hdcc.com>
2013-08-28 08:52:34 -1000

    Adding pages and animations

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,33 @@
 
 $(function(){
 	//alert("working");
+
+
+	var isworking = false;
+	var projectClicked = false;
 	
+	$('div.project')
+	.mouseover(function(){ 
+		if(isworking == false && projectClicked == false) { 
+			//$(this).hasClass('moved') == false        //condition if the element hasn't been hovered over and you hover over from a separate project.
+			isworking = true; 
+			$(this).animate({ height: "+=25px"}, 150, function(){ isworking=false; }); 
+			} 
+	})
+	.mouseout(function(){ 
+		if(isworking == false && projectClicked == false) { 
+			isworking = true; 
+			$(this).animate({ height: "-=25px"}, 150, function(){ isworking=false;} ); 
+		}
+	})
+	.click(function(){ 
+		//toggle open
+		if (projectClicked == false) {
+			projectClicked = true;
+			$(this).children(".hidden").fadeIn('fast', function(){
+				return 0;
+			});
+		}
+	});
 	
 });
