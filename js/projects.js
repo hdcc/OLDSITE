9ae4d99bfd40a17975f17fad55296354f86430b7
hdcc <dwilliams@hdcc.com>
2013-09-11 10:27:05 -1000

    refining the layout and adding images

--- js/projects.js
+++ js/projects.js
@@ -13,32 +13,32 @@ $(function(){
 
 
 	// FUNCTION DEFS
-	function hovEffect(elem, status){
-		if (status == false && elem.hasClass("clicked") == false ) { 
-			elem.filter(':not(:animated)').animate({ height: "+=55px"}, 200);
-		}
-		else if (status == true && elem.hasClass("clicked") == false){ 
-			elem.filter(':not(:animated)').animate({ height: "-=55px"}, 200); 
-		} 
-		return 0;
-	}
+	// function hovEffect(elem, status){
+	// 	if (status == false && elem.hasClass("clicked") == false ) { 
+	// 		elem.filter(':not(:animated)').animate({ height: "+=35px"}, 200);
+	// 	}
+	// 	else if (status == true && elem.hasClass("clicked") == false){ 
+	// 		elem.filter(':not(:animated)').animate({ height: "-=35px"}, 200); 
+	// 	} 
+	// 	return 0;
+	// }
 
 
 
 
 	// PROJECT ELEM FUNCTIONALITY
-	$('div.project:not(.loadmore)')
-	.mouseenter(function(){ hovEffect($(this), false); })
-	.mouseleave(function(){ hovEffect($(this), true); })
-	.click(function(){ 
-		//toggle open
-		if (projectClicked == false) { //use class names to check for err
-			  //use class names to check for err
-			$(this).animate({ height: "+=300px" }, 200, function(){ 
-				$(this).addClass("clicked").children(".hidden").fadeIn('fast');
-			});
-		}
-	});
+	// $('div.project:not(.loadmore)')
+	// .mouseenter(function(){ hovEffect($(this), false); })
+	// .mouseleave(function(){ hovEffect($(this), true); })
+	// .click(function(){ 
+	// 	//toggle open
+	// 	if (projectClicked == false) { //use class names to check for err
+	// 		  //use class names to check for err
+	// 		// $(this).slideDown({ height: "+=300px" }, 200, function(){ 
+	// 			// $(this).addClass("clicked").children(".hidden").fadeIn('fast');
+	// 		// });
+	// 	}
+	// });
 
 
 
@@ -46,18 +46,20 @@ $(function(){
 
 
 	// DIVISION ELEM FUNCTIONALITY
-	$('div.division:not(.loadmore)')
-	.mouseenter(function(){ hovEffect($(this), false); })
-	.mouseleave(function() { hovEffect($(this), true); })
-	.click(function() {
-		if (projectClicked == false) { //use class names to handle this error check
-			//use class names to check for error
-			$(this).animate({ height: "+=300px" }, 300, function(){ 
-				$(this).addClass("clicked");
-			});
-			//animate the box down to show the content underneath
-		}
-	})
+	// $('div.division:not(.loadmore)')
+	// .mouseenter(function(){ hovEffect($(this), false); })
+	// .mouseleave(function() { hovEffect($(this), true); })
+	// .click(function() {
+	// 	if (projectClicked == false) { //use class names to handle this error check
+	// 		//use class names to check for error
+	// 		// $(this).slideDown('slow', function(){ 
+	// 			// $(this).addClass("clicked");
+	// 		// });
+	// 		//animate the box down to show the content underneath
+	// 	}
+	// })
+
+	// fire off a click event on the elements btn element to drop down and reveal extra content
 
 
 
@@ -80,7 +82,6 @@ $(function(){
 	
 
 	//instead of hover states with subtle dropdown, do a hover effect with an outer radial gradient glow on both sides of div 
-
 
 
 });
