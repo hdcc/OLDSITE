e087d11aee5959e475174d5dd4d6390fed57ffc2
hdcc <dwilliamshdcc@gmail.com>
2013-09-24 15:01:11 -1000

    updated padding on home page. removed unused js

--- js/projects.js
+++ js/projects.js
@@ -9,59 +9,6 @@ $(function(){
 
 
 
-	//missing way to determine if mouse has left one elem and entered another before triggering the mouseover event 
-
-
-	// FUNCTION DEFS
-	// function hovEffect(elem, status){
-	// 	if (status == false && elem.hasClass("clicked") == false ) { 
-	// 		elem.filter(':not(:animated)').animate({ height: "+=35px"}, 200);
-	// 	}
-	// 	else if (status == true && elem.hasClass("clicked") == false){ 
-	// 		elem.filter(':not(:animated)').animate({ height: "-=35px"}, 200); 
-	// 	} 
-	// 	return 0;
-	// }
-
-
-
-
-	// PROJECT ELEM FUNCTIONALITY
-	// $('div.project:not(.loadmore)')
-	// .mouseenter(function(){ hovEffect($(this), false); })
-	// .mouseleave(function(){ hovEffect($(this), true); })
-	// .click(function(){ 
-	// 	//toggle open
-	// 	if (projectClicked == false) { //use class names to check for err
-	// 		  //use class names to check for err
-	// 		// $(this).slideDown({ height: "+=300px" }, 200, function(){ 
-	// 			// $(this).addClass("clicked").children(".hidden").fadeIn('fast');
-	// 		// });
-	// 	}
-	// });
-
-
-
-
-
-
-	// DIVISION ELEM FUNCTIONALITY
-	// $('div.division:not(.loadmore)')
-	// .mouseenter(function(){ hovEffect($(this), false); })
-	// .mouseleave(function() { hovEffect($(this), true); })
-	// .click(function() {
-	// 	if (projectClicked == false) { //use class names to handle this error check
-	// 		//use class names to check for error
-	// 		// $(this).slideDown('slow', function(){ 
-	// 			// $(this).addClass("clicked");
-	// 		// });
-	// 		//animate the box down to show the content underneath
-	// 	}
-	// })
-
-	// fire off a click event on the elements btn element to drop down and reveal extra content
-	
-
 	//the see more button, or any element of the division div is clicked
 	$('.divisionbtn').click(function(e){
 		e.preventDefault();
@@ -69,18 +16,18 @@ $(function(){
 	});
 
 
+
 	//carousel slider on home page
 	$('#preview').carousel({
 		interval: 6000,
 		pause: "hover"
 	});
+
 	
 	//set clicks on carousel a navigators
 	$('.carousel-control.left').click(function() { $('#preview').carousel('prev'); });
 	$('.carousel-control.right').click(function() { $('#preview').carousel('next'); });
 	
-
-	//instead of hover states with subtle dropdown, do a hover effect with an outer radial gradient glow on both sides of div 
 
 
 	$('.tilt')
@@ -100,21 +47,17 @@ $(function(){
 		$(this).slideUp('fast').parent('.tilt').click();
 	});
 
-	// $('.project').hover(function(){
-	// 	if($(this).hasClass('tilt') == true) {
-	// 		$(this).css('cursor', 'pointer');
-	// 	}
-	// });
 
 	$('.loadbtn').click(function(e){
-		//e.stopPropagation();
 		e.preventDefault();
 		$(this).parent('.loadmore').find('> .hide').slideDown(500);
 		$(this).slideUp('fast');
 	});
 
+
 	$('.fancybox').fancybox();
 	
+
 	$('.img-can a')
 	.mouseenter(function(e){ 
 		e.preventDefault();
@@ -123,5 +66,6 @@ $(function(){
 		e.preventDefault();
 		$(this).stop().fadeTo('fast', .6);
 	});
+
 
 });
