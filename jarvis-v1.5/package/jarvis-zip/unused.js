c5cf215873d3c82a8935b89e3f403f4b5463003f
hdcc <dwilliamshdcc@gmail.com>
2013-11-04 11:00:13 -1000

    adding a theme to home page
    parallax theme

---
+++ jarvis-v1.5/package/jarvis-zip/unused.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+
+/*	
+	var height_video = $(window).width();
+	var height_responsive = (height_video / 1.78011) + 1;
+	var window_height = $(window).height();
+	
+	
+	function fullscreenImgHeight(){ 
+		$('#home').css({height:window_height}); 
+		//$('body').css({height:window_height});
+	}
+	
+	fullscreenImgHeight();
+	
+	$(window).bind('resize',function() {
+		fullscreenImgHeight();
+	});	
+	
+	$(window).resize(function() {
+		var height_video = $(window).width();
+		var height_responsive = (height_video / 1.78011) + 1;
+	});
+	*/
+	
+	//homepage flexslider
+/*	$('#carousel_msg').flexslider({						
+		animation: "swing",
+		direction: "vertical", 
+		slideshow: true,
+		slideshowSpeed: 3500,
+		animationDuration: 1000,
+		directionNav: false,
+		controlNav: true,
+		smootheHeight:true,
+		after: function(slider) {
+		  slider.removeClass('selected');
+		}	
+ });*/
+	
--- js/homepage.js
+++ js/homepage.js
@@ -1,34 +1,12 @@
 // JavaScript Document
 
 $(function(){
-	
-	var height_video = $(window).width();
-	var height_responsive = (height_video / 1.78011) + 1;
-	var window_height = $(window).height();
-	
-	
-	function fullscreenImgHeight(){ 
-		$('#home').css({height:window_height}); 
-		//$('body').css({height:window_height});
-	}
-	
-	fullscreenImgHeight();
-	
-	$(window).bind('resize',function() {
-		fullscreenImgHeight();
-	});	
-	
-	$(window).resize(function() {
-		var height_video = $(window).width();
-		var height_responsive = (height_video / 1.78011) + 1;
-	});
-	
-	
+
 	//randomly assign home page backgrounds
-	var range = Math.floor((Math.random()*4)+1);
+	var range = Math.floor((Math.random()*5)+1); 
 	var imagePath = '"images/homepage_bkg_';
 	function randomBackground(){
-		$('#home').css('background-image', 'url(' + imagePath + range + '.png")');
+		$('.home-parallax').css('background-image', 'url(' + imagePath + range + '.png")');
 	}
 	randomBackground();
 	
@@ -37,5 +15,7 @@ $(function(){
 	//message displays. black background with clear message. 
 	//background absolutely positioned with black color
 	//the background fades out.
+	
+	
 	
 });
